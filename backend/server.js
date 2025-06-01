306c558df81f9aab49c3306cceb9d2948fbb800d
xbbbbbbbbbbbb <297906031@qq.com>
2025-06-01 03:13:20 +0200

    fix path

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.options('/fastapi/*', (req, res) => {
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("/{*splat}", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
